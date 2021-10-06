b5a220ec6c967e52e5fdcaf288a071b1ca5a102f
Roberth Morales <robinmoralesquiroz@gmail.com>
2021-10-06 00:00:53 -0500

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { LoginComponent } from './auth/login/login.component';
+import { NopagefountComponent } from './pages/nopagefount/nopagefount.component';
+import { BreadcrumbsComponent } from './share/breadcrumbs/breadcrumbs.component';
+import { SidebarComponent } from './share/sidebar/sidebar.component';
+import { HeaderComponent } from './share/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RegisterComponent,
+    LoginComponent,
+    NopagefountComponent,
+    BreadcrumbsComponent,
+    SidebarComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
