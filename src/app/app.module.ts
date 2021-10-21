bd437d4e2953ce84bf667f05d58d6c53c4b63952
Roberth <robinmoralesquiroz@gmail.com>
2021-10-21 00:06:53 -0500

    the modularization of the components begins

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,61 +1,28 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { Routes, RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
+//Modules
+import { PagesModule } from './pages/pages.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AuthModule } from './auth/auth.module';
 
 import { AppComponent } from './app.component';
-import { RegisterComponent } from './auth/register/register.component';
-import { LoginComponent } from './auth/login/login.component';
-import { NopagefountComponent } from './pages/nopagefount/nopagefount.component';
-import { BreadcrumbsComponent } from './share/breadcrumbs/breadcrumbs.component';
-import { SidebarComponent } from './share/sidebar/sidebar.component';
-import { HeaderComponent } from './share/header/header.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
-import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
-import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { Grafica1Component } from './pages/grafica1/grafica1.component';
-import { AppRoutingModule } from './app-routing.module';
-import { PagesComponent } from './pages/pages.component';
-
-const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
-  suppressScrollX: true,
-  wheelSpeed: 1,
-  wheelPropagation: true,
-  minScrollbarLength: 20
-};
+import { NopagefountComponent } from './nopagefount/nopagefount.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RegisterComponent,
-    LoginComponent,
     NopagefountComponent,
-    BreadcrumbsComponent,
-    SidebarComponent,
-    HeaderComponent,
-    DashboardComponent,
-    ProgressComponent,
-    Grafica1Component,
-    PagesComponent,
-
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    NgbModule,
-	  PerfectScrollbarModule,
-    AppRoutingModule
+    AppRoutingModule,
+    PagesModule,
+    AuthModule
   ],
-  providers: [
-    {
-      provide: PERFECT_SCROLLBAR_CONFIG,
-      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
-    }
-  ],
+
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/pages/pages.module.ts
@@ -0,0 +1,51 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { RouterModule } from '@angular/router'
+
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProgressComponent } from './progress/progress.component';
+import { Grafica1Component } from './grafica1/grafica1.component';
+import { PagesComponent } from './pages.component';
+import { ShareModule } from '../share/share.module';
+//import { AppRoutingModule } from '../app-routing.module';
+import { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+import { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+import { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
+const DEFAULT_PERFECT_SCROLLBAR_CONFIG: PerfectScrollbarConfigInterface = {
+  suppressScrollX: true,
+  wheelSpeed: 1,
+  wheelPropagation: true,
+  minScrollbarLength: 20
+};
+
+
+
+@NgModule({
+  declarations: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+  ],
+  exports: [
+    DashboardComponent,
+    ProgressComponent,
+    Grafica1Component,
+    PagesComponent,
+  ],
+  imports: [
+    CommonModule,
+    ShareModule,
+	  PerfectScrollbarModule,
+    RouterModule
+  ],
+  providers: [
+    {
+      provide: PERFECT_SCROLLBAR_CONFIG,
+      useValue: DEFAULT_PERFECT_SCROLLBAR_CONFIG
+    }
+  ],
+})
+export class PagesModule { }
